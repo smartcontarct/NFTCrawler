c63e1306e1be45706ee33a26f2669b08ba21d74a
Fatemeh <fatemeh.heidari-soureshjani@polymtl.ca>
2022-01-16 11:45:16 -0500

    MINT

--- src/pages/mint.js
+++ src/pages/mint.js
@@ -1,43 +1,81 @@
 import React, { Component } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class Mint extends Component {
-    listAll = async (lnft) => {
-        fetch('https://api.nftport.xyz/v0/me/mints?chain=polygon', {
-          "method": "GET",
-          "headers": {
-            "Content-Type": "application/json",
-            "Authorization": "9dd184c0-2c6f-46a0-ac99-bbf92cf18951"
-          }
-        })
-        
-        .then(response => {
-            console.log(response);
-          })
-        .catch(err => {
-          console.error(err);
-        });
-        };
+  listAll = async (lnft) => {
+    fetch('https://api.nftport.xyz/v0/me/mints?chain=polygon', {
+      "method": "GET",
+      "headers": {
+        "Content-Type": "application/json",
+        "Authorization": "9dd184c0-2c6f-46a0-ac99-bbf92cf18951"
+      }
+    })
 
-    render() {
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
-        return (
-            <div>
-                <Form >
-          
-            <Button variant="primary"  onClick={this.listAll}>
+  render() {
+
+    return (
+      <div>
+        <Form >
+          <Form.Group as={Row} className="mb-3" controlId="formKB9Response">
+            <Form.Label column sm="2">
+              Input Address:
+                </Form.Label>
+            <Col sm="5">
+              <Form.Control type="searchAddress" placeholder="Address" ref={this.searchAddress} />
+            </Col>
+          </Form.Group>
+          <br></br>
+          <Row>
+            <Col>
+              <Form.Group as={Row} className="mb-3" controlId="formKB9Response">
+                <Form.Label column sm="4">
+                  NFT Name:
+                </Form.Label>
+                <Col sm="3">
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formKB9Response">
+                <Form.Label column sm="4">
+                  NFT Description:
+                </Form.Label>
+                <Col sm="3">
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row} className="mb-3" controlId="formKB9Response">
+                <Form.Label column sm="4">
+                  File URL:
+                </Form.Label>
+                <Col sm="3">
+                </Col>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button variant="primary" type="submit" onClick={this.fetchData}>Mint</Button>
+            </Col>
+          </Row>
+          <Button variant="primary" onClick={this.listAll}>
             list All Your Minted NFTs
 	        </Button>
 
 
-            
-            </Form>
-       
-            </div>
 
-        )
-    }
+        </Form>
+
+      </div>
+
+    )
+  }
 
 }
 export default Mint;
